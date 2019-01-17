760c939c172a4496a1abb972553237c7c186a7ce
Wes Peter <wes@wespeter.com>
2019-01-17 09:59:03 -0800

    wip - before moving to rollup

--- lib/side-drawer.ts
+++ lib/side-drawer.ts
@@ -1,25 +1,18 @@
+import style from "./side-drawer.css";
+
+declare var html: any;
+
 export default class SideDrawer extends HTMLElement {
   constructor() {
     super();
 
     this.attachShadow({ mode: "open" });
-    // @ts-ignore
-    this.shadowRoot.innerHTML = html`
+    this.shadowRoot!.innerHTML = html`
       <style>
-        .container {
-          height: 100%;
-          width: 0;
-          position: fixed;
-          z-index: 1;
-          top: 0;
-          left: 0;
-          overflow-x: hidden;
-          transition: 0.5s;
-        }
+        ${style}
       </style>
 
       <div class="container"><slot></slot></div>
-      >
     `;
   }
 }
