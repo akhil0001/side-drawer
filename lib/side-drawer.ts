e515dc2ca6e4a7ef21f80a1e928b84c3154f9022
Wes Peter <wes@wespeter.com>
2019-01-18 08:29:59 -0800

    using template

--- lib/side-drawer.ts
+++ lib/side-drawer.ts
@@ -1,20 +1,22 @@
 // @ts-ignore
 import style from "./side-drawer.css";
 
-declare var html: any;
+// declare var html: any;
 
-export default class SideDrawer extends HTMLElement {
+let tmpl = document.createElement("template");
+tmpl.innerHTML = `
+  <style>
+    ${style}
+  </style>
+  <div class="container"><slot></slot></div>
+`;
+
+export class SideDrawer extends HTMLElement {
   constructor() {
     super();
 
-    this.attachShadow({ mode: "open" });
-    this.shadowRoot!.innerHTML = html`
-      <style>
-        ${style}
-      </style>
-
-      <div class="container"><slot></slot></div>
-    `;
+    const shadowRoot = this.attachShadow({ mode: "open" });
+    shadowRoot.appendChild(tmpl.content.cloneNode(true));
   }
 }
 
