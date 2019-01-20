abb3a0ada32f7f22570670bd034a645668fc70fd
Wes Peter <wes@wespeter.com>
2019-01-20 14:25:51 -0800

    before using transform

--- lib/side-drawer.ts
+++ lib/side-drawer.ts
@@ -10,11 +10,23 @@ const tmpl = document.createElement("template");
 tmpl.innerHTML = `<style>${style}</style>${template}`;
 
 export class SideDrawer extends HTMLElement {
+  private _freeSpaceDiv: HTMLElement | null;
+
   constructor() {
     super();
 
     const shadowRoot = this.attachShadow({ mode: "open" });
     shadowRoot.appendChild(tmpl.content.cloneNode(true));
+    this._freeSpaceDiv = shadowRoot.getElementById("fs");
+  }
+
+  connectedCallback() {
+    if (this._freeSpaceDiv) {
+      this._freeSpaceDiv.addEventListener(
+        "click",
+        this.handleFreeSpaceDivClick
+      );
+    }
   }
 
   get open() {
@@ -29,20 +41,24 @@ export class SideDrawer extends HTMLElement {
     }
   }
 
-  static get observedAttributes() {
-    return ["open"];
-  }
+  // static get observedAttributes() {
+  //   return ["open"];
+  // }
 
-  attributeChangedCallback(_name: string, _oldValue: any, _newValue: any) {
-    // When the drawer is closed, update keyboard/screen reader behavior.
-    if (!this.open) {
-      this.setAttribute("tabindex", "-1");
-      this.setAttribute("aria-disabled", "true");
-    } else {
-      this.setAttribute("tabindex", "0");
-      this.setAttribute("aria-disabled", "false");
-    }
-  }
+  // attributeChangedCallback(_name: string, _oldValue: any, _newValue: any) {
+  //   // When the drawer is closed, update keyboard/screen reader behavior.
+  //   if (!this.open) {
+  //     this.setAttribute("tabindex", "-1");
+  //     this.setAttribute("aria-disabled", "true");
+  //   } else {
+  //     this.setAttribute("tabindex", "0");
+  //     this.setAttribute("aria-disabled", "false");
+  //   }
+  // }
+
+  private handleFreeSpaceDivClick = (_e: any) => {
+    this.open = false;
+  };
 }
 
 customElements.define("side-drawer", SideDrawer);
